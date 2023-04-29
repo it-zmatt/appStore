7d99f2b8cf4a317e83354726f82e3aefa1d9b439
it_zmatt <mohamed.taous@univ-khenchela.dz>
2023-04-29 20:10:05 +0200

    Created the home page and Register page and done with mapping them

--- thehive/registration/views.py
+++ thehive/registration/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(response):
+    return render(response, 'registration/base.html')
+
+def register(response):
+    return render(response, 'registration/index.html')
+
