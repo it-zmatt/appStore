5be853f6d38f6ea528037c9b7006e31b555f6236
it_zmatt <mohamed.taous@univ-khenchela.dz>
2023-04-29 23:36:25 +0200

    Created the model User Profile

--- thehive/accounts/models.py
+++ thehive/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.user.username
