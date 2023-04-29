425429aba9d8742fb93b99eb13b31670c0ea349c
it_zmatt <mohamed.taous@univ-khenchela.dz>
2023-04-30 00:12:58 +0200

    Created a model supplier

--- thehive/accounts/models.py
+++ thehive/accounts/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    address = models.CharField(max_length=200)
     
     def __str__(self):
         return self.user.username
--- thehive/supplier/models.py
+++ thehive/supplier/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from accounts.models import UserProfile
 
 # Create your models here.
+class Supplier(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    user_profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
