c4ef5b2688cbf91ebe85cf6ff98f479f62a6fc7e
it_zmatt <mohamed.taous@univ-khenchela.dz>
2023-05-05 15:26:22 +0200

    Styling the navbar of Home Page

--- thehive/registration/views.py
+++ thehive/registration/views.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.shortcuts import render, redirect
 from .forms import RegistrForm
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.models import User
+
 
 
 # Create your views here.
@@ -16,8 +18,16 @@ def register(response):
     if response.method == 'POST':
         form = RegistrForm(response.POST)
         if form.is_valid():
-            form.save()
-        return redirect('Login')          
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            if User.objects.filter(username=username).exists():
+                form.add_error('username', 'This username is already taken.')
+            elif User.objects.filter(email=email).exists():
+                form.add_error('email', 'This email is already taken.')
+            else:
+                form.save()
+                return redirect('login')
+
     else:
          form = RegistrForm()
     return render(response, 'registration/index.html', {'form': form})
